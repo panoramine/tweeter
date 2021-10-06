6a8dfdd875f6c0835e78aa8293e001fa3dcce22c
panoramine <camila.c.a.salles@gmail.com>
2021-10-05 21:22:57 -0700

    tweeter project

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $("#tweet-text").on('input propertychange',  function() {
+
+    let txtContent = $('#tweet-text').val();
+    let strCount = (140 - txtContent.length);     
+
+    $(".counter").val(strCount);
+
+    if (strCount < 0) {
+      return $(".counter").addClass("danger");
+    } else {
+      $(".counter").removeClass("danger");
+    }
+  });
+});
