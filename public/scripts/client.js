21effb9645c932dd8f17f859af4d4e150cfe73ef
panoramine <camila.c.a.salles@gmail.com>
2021-10-06 18:50:03 -0700

    add loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
   
   const renderTweets = function(tweets) {
  
-    tweets.forEach((tweet) => {
+    tweets.reverse().forEach((tweet) => {
       $tweetHolder = createTweetElement(tweet);
   
       $(".tweet-container").append($tweetHolder);
@@ -55,39 +55,10 @@ $(document).ready(function() {
   
   };
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  renderTweets(data);
-
   $tweetData = $(this).serialize();
 
   $("form").submit(function(event) {
     event.preventDefault();
-
-    // console.log($(this).serialize())
 
   $.ajax({
     type: "POST",
@@ -96,6 +67,19 @@ $(document).ready(function() {
   });
   
   });
+
+  const loadTweets = function() {
+
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:8080/tweets"
+    })
+    .then((result) => {
+      console.log(result)
+      renderTweets(result)
+    })
+  }
+  loadTweets()
 });
 
 
